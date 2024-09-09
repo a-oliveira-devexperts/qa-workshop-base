a5d611e7594e0aec3ccc026ee20af6b36fc15182
Nikita Tatyannikov <ntatyannikov@devexperts.com>
2024-09-09 11:55:14 +0100

    Add raw workflow + some test to check the playwright deps

--- playwright-workshop/src/test/java/org/example/ExampleTest.java
+++ playwright-workshop/src/test/java/org/example/ExampleTest.java
@@ -1,6 +1,11 @@
 package org.example;
 
+import com.microsoft.playwright.Browser;
+import com.microsoft.playwright.Page;
+import com.microsoft.playwright.Playwright;
 import org.junit.jupiter.api.Test;
+
+import java.nio.file.Paths;
 
 public class ExampleTest {
     //
@@ -8,5 +13,16 @@ public class ExampleTest {
     @Test
     public void example(){
 
+    }
+
+    @Test
+    void testFirst() {
+        try (Playwright playwright = Playwright.create();
+             Browser browser = playwright.chromium().launch();
+             Page page = browser.newPage()) {
+            page.navigate("https://qa-testing.in.devexperts.com/internship/");
+            page.screenshot(new Page.ScreenshotOptions().setPath(Paths.get("example.png")));
+            System.out.println("ECHO: " + page.title());
+        }
     }
 }
