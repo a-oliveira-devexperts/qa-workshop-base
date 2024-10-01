6538985ce4f52c18095b05fcd15ce6c8e7bbca11
Nikita Tatyannikov <ntatyannikov@devexperts.com>
2024-10-01 19:40:02 +0100

    Pre-workshop CI dry run

--- playwright-workshop/src/test/java/org/example/ExampleTest.java
+++ playwright-workshop/src/test/java/org/example/ExampleTest.java
@@ -1,12 +1,55 @@
 package org.example;
 
+import com.microsoft.playwright.Browser;
+import com.microsoft.playwright.Locator;
+import com.microsoft.playwright.Page;
+import com.microsoft.playwright.Playwright;
+import com.microsoft.playwright.options.AriaRole;
 import org.junit.jupiter.api.Test;
 
-public class ExampleTest {
-    //
-    //
-    @Test
-    public void example(){
+import static com.microsoft.playwright.assertions.PlaywrightAssertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertAll;
 
+public class ExampleTest {
+
+    @Test
+    public void testLoginSuccess() {
+        //Setup
+        //Create Playwright
+        Playwright playwright = Playwright.create();
+        //Create Browser
+        Browser browser = playwright.chromium().launch();
+        //Create Page
+        Page page = browser.newPage();
+        page.navigate("https://qa-testing.in.devexperts.com/internship/");
+
+        //Test Cases
+        //set Username
+        Locator inputUsername = page.getByPlaceholder("Username");
+        inputUsername.fill("a.oliveira@devexperts.com");
+        //set password
+        //Locator inputPassword = page.getByPlaceholder("Password" , new Page.GetByPlaceholderOptions().setExact(true));
+        Locator inputPassword = page.locator("#password");
+        inputPassword.fill("Andre123456@");
+        //click in Login Button
+        Locator loginButton = page.getByRole(AriaRole.BUTTON, new Page.GetByRoleOptions().setName("Login"));
+        loginButton.click();
+        //Check if the login button redirected to the home page.
+
+        Locator homePageHeader = page.locator(".header-title-content");
+        Locator tradePanelHeader = page.locator("#trade-title");
+
+        //hard assertion
+        //assertThat(homePageHeader).hasText("Home Test FFF Task");
+        //assertThat(tradePanelHeader).hasText("TrFFFFade");
+        //soft assertion
+        assertAll("Login Assertion",
+                () -> assertThat(homePageHeader).hasText("Home Test Task"),
+                () -> assertThat(tradePanelHeader).hasText("Trade"));
+
+        //close everything
+        page.close();
+        browser.close();
+        playwright.close();
     }
 }
