e8d0ebeb29bc19281925aab69b86873356ce3ee4
Konstantin Markov <kmarkov@devexperts.com>
2024-10-09 12:43:22 +0100

    1st test created on Workshop

--- playwright-workshop/src/test/java/com/devexperts/in/qatesting/ExampleTest.java
+++ playwright-workshop/src/test/java/com/devexperts/in/qatesting/ExampleTest.java
@@ -1,4 +1,4 @@
-package org.example;
+package com.devexperts.in.qatesting;
 
 import org.junit.jupiter.api.Test;
 
---
+++ playwright-workshop/src/test/java/com/devexperts/in/qatesting/LoginTest.java
@@ -0,0 +1,53 @@
+package com.devexperts.in.qatesting;
+
+import com.microsoft.playwright.*;
+import com.microsoft.playwright.options.AriaRole;
+import org.junit.jupiter.api.Test;
+
+import static com.microsoft.playwright.assertions.PlaywrightAssertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class LoginTest {
+
+    @Test
+    public void testSuccessfulLogin(){
+        //Setup
+        //Create Playwright, Browser, Page
+        Playwright playwright = Playwright.create();
+        Browser browser = playwright.chromium().launch(new BrowserType.LaunchOptions().setHeadless(false));
+        Page page = browser.newPage();
+        //Open Login Page
+        page.navigate("https://qa-testing.in.devexperts.com/internship/");
+        //Inform Username
+        Locator inputUsername = page.getByPlaceholder("Username");
+        inputUsername.fill("kmarkov@devexperts.com");
+        //Inform Password
+        //Locator inputPassword = page.getByPlaceholder("Password", new Page.GetByPlaceholderOptions().setExact(true));
+        Locator inputPassword = page.locator("#password");
+        inputPassword.fill("Aa12345678!");
+        //Click Login Button
+        Locator loginButton = page.getByRole( AriaRole.BUTTON,  new Page.GetByRoleOptions().setName("Login"));
+        loginButton.click();
+        //Check if we were redirected to the home page
+        Locator homeHeaderPage = page.locator(".header-title-content");
+
+        //Type1
+            //assertThat(homeHeaderPage).hasText("Home TestD Task");
+            //assertThat(homeHeaderPage).isVisible();
+
+        //Type2
+            //assertAll("Login Page Checks",
+            //        () -> assertEquals("Home Test Task", homeHeaderPage.innerText()),
+            //        () -> assertTrue(homeHeaderPage.isVisible()));
+
+        //Type3
+            assertAll("Login Page Checks",
+                    () -> assertThat(homeHeaderPage).hasText("Home Test Task"),
+                    () -> assertThat(homeHeaderPage).isVisible());
+        //Close everything
+        page.close();
+        browser.close();
+        playwright.close();
+    }
+
+}
