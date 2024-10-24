a31455fe1b28999d9599684d6ed28cf16bb76699
Arevik Gimishyan <agimishyan@devexperts.com>
2024-10-24 21:47:56 +0300

    QEXDEV-332 - Added JUnit Annotations

--- playwright-workshop/src/test/java/com/devexperts/in/qatesting/LoginTest.java
+++ playwright-workshop/src/test/java/com/devexperts/in/qatesting/LoginTest.java
@@ -2,42 +2,55 @@ package com.devexperts.in.qatesting;
 
 import com.microsoft.playwright.*;
 import com.microsoft.playwright.options.AriaRole;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 
 import static com.microsoft.playwright.assertions.PlaywrightAssertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertAll;
 
 public class LoginTest {
+
+    private static Playwright playwright;
+    private Browser browser;
+    private Page page;
+
+    @BeforeAll
+    public static void createPlaywright(){
+        playwright = Playwright.create();
+    }
+
+    @BeforeEach
+    public void setUp(){
+        //Setup: create Browser, Page
+        browser = playwright.chromium().launch();
+        //browser = playwright.chromium().launch(new BrowserType.LaunchOptions().setHeadless(false));
+        page = browser.newPage();
+        page.navigate("https://qa-testing.in.devexperts.com/internship/");
+    }
+
     @Test
     public void testSuccessfulLogin(){
-        //Setup
-        //Create Playwright, Browser, Page
-        Playwright playwright = Playwright.create();
-        Browser browser = playwright.chromium().launch();
-        //Browser browser = playwright.chromium().launch(new BrowserType.LaunchOptions().setHeadless(false));
-        Page page = browser.newPage();
-        //Open Login Page
-        page.navigate("https://qa-testing.in.devexperts.com/internship/");
-        //Inform username
+        //Enter Username and Password and Log in
         Locator inputUsername = page.getByPlaceholder("Username");
         inputUsername.fill("gmshn.edu@gmail.com");
-        //Inform password
         Locator inputPassword = page.locator("#password");
-        //Locator inputPassword = page.getByPlaceholder("Password" , new Page.GetByPlaceholderOptions().setExact(true));
         inputPassword.fill("autoQAedu@2024");
-        //Click Login Button
         Locator loginButton = page.getByRole(AriaRole.BUTTON , new Page.GetByRoleOptions().setName("Login"));
         loginButton.click();
-        //Check if we were redirected to the home
+        //Check that we are redirected to home page
         Locator homeHeaderPage = page.locator(".header-title-content");
-        //assertThat(homeHeaderPage).isVisible();
-        //assertThat(homeHeaderPage).hasText("Home Test Task");
         assertAll("Login Page Checks",
                 () ->assertThat(homeHeaderPage).hasText("Home Test Task"),
                 () ->assertThat(homeHeaderPage).isVisible());
-        //Close everything
+    }
+
+    @AfterEach
+    public void tearDown(){
         page.close();
         browser.close();
+    }
+
+    @AfterAll
+    public static void closePlaywright(){
         playwright.close();
     }
 }
