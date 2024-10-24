5c4f0751ee7a77f30c1955a6437548852286ae84
Mihail Rangelov <mrangelov@devexperts.com>
2024-10-24 14:59:49 +0300

    [QEXDEV-336] - Added Test Successful Login

--- playwright-workshop/src/test/java/com/devexperts/in/qatesting/LoginTest.java
+++ playwright-workshop/src/test/java/com/devexperts/in/qatesting/LoginTest.java
@@ -11,33 +11,21 @@ public class LoginTest {
 
     @Test
     public void testSuccessfulLogin(){
-        //Setup
-        //Playwright, Browser, Page
         Playwright playwright = Playwright.create();
         Browser browser = playwright.chromium().launch(new BrowserType.LaunchOptions().setHeadless(false));
         Page page = browser.newPage();
-        //Open Login Page
         page.navigate("https://qa-testing.in.devexperts.com/internship/");
-        //Inform username
         Locator inputUsername = page.getByPlaceholder("Username");
         inputUsername.fill("mrdxtm4_1@proton.me");
-        //Inform password
-        //Locator inputPassword = page.getByPlaceholder("Password", new Page.GetByPlaceholderOptions().setExact(true));
         Locator inputPassword = page.locator("#password");
         inputPassword.fill("Qw#45678");
-        //Click Login Button
         Locator loginButton = page.getByRole(AriaRole.BUTTON, new Page.GetByRoleOptions().setName("Login"));
         loginButton.click();
-        //Check if we were redirected to the home page
         Locator homeHeaderPage = page.locator(".header-title-content");
-//        assertThat(homeHeaderPage).hasText("Home Test Task");
-//        assertThat(homeHeaderPage).isVisible();
         assertAll("Login Page Checks" ,
-//                () -> assertEquals("HOME TEST TASK", homeHeaderPage.innerText()),
-//                () -> assertTrue(homeHeaderPage.isVisible()));
                 () ->assertThat(homeHeaderPage).hasText("Home Test Task") ,
                 () -> assertThat(homeHeaderPage).isVisible());
-        //Close everything
+
         page.close();
         browser.close();
         playwright.close();
