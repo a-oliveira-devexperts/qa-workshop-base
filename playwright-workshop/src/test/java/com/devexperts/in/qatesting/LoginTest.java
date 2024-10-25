80ca7b692ff8534b27719ce751e16fad49dfed13
Rita Ferraz <rferraz@devexperts.com>
2024-10-25 14:55:27 +0100

    QEXDEV-354 - Removed headless configuration

--- playwright-workshop/src/test/java/com/devexperts/in/qatesting/LoginTest.java
+++ playwright-workshop/src/test/java/com/devexperts/in/qatesting/LoginTest.java
@@ -8,14 +8,11 @@ import static com.microsoft.playwright.assertions.PlaywrightAssertions.assertTha
 import static org.junit.jupiter.api.Assertions.assertAll;
 
 public class LoginTest {
-
     @Test
     public void testSuccessfulLogin() {
-        //Setup Playwright, Browser, Page
         Playwright playwright = Playwright.create();
-        Browser browser = playwright.chromium().launch(new BrowserType.LaunchOptions().setHeadless(false));
+        Browser browser = playwright.chromium().launch();
         Page page = browser.newPage();
-
         page.navigate("https://qa-testing.in.devexperts.com/internship/");
 
         Locator inputUsername = page.getByPlaceholder("Username");
@@ -29,13 +26,11 @@ public class LoginTest {
         Locator buttonLogin = page.getByRole(AriaRole.BUTTON, new Page.GetByRoleOptions().setName("Login")); //find elements by their role on the page. find a button that is named logjn
         buttonLogin.click();
 
-
         Locator homeHeader = page.locator(".header-title-content");
         //soft assertions, will only fail after all checks
         assertAll("Login Checks",
                 ()-> assertThat(homeHeader).hasText("Home Test Task"),
                 ()-> assertThat(homeHeader).isVisible());
-        //Close page, browser and playwright
         page.close();
         browser.close();
         playwright.close();
