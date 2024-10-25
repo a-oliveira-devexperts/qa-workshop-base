4eda9551808e77b905c454822a8162a0ebdd9951
Rita Ferraz <rferraz@devexperts.com>
2024-10-25 15:30:38 +0100

    QEXDEV-354 Removed comments Refactor code Create testLoginWithWrongCredentials

--- playwright-workshop/src/test/java/com/devexperts/in/qatesting/LoginTest.java
+++ playwright-workshop/src/test/java/com/devexperts/in/qatesting/LoginTest.java
@@ -2,37 +2,53 @@ package com.devexperts.in.qatesting;
 
 import com.microsoft.playwright.*;
 import com.microsoft.playwright.options.AriaRole;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 
 import static com.microsoft.playwright.assertions.PlaywrightAssertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertAll;
 
 public class LoginTest {
+    private static Playwright playwright;
+    private Browser browser;
+    private Page page;
+
+    @BeforeAll
+    public static void beforeAll(){
+        playwright = Playwright.create();
+    }
+
+    @BeforeEach
+    public void setUp(){
+        browser = playwright.chromium().launch();
+        page = browser.newPage();
+        page.navigate("https://qa-testing.in.devexperts.com/internship/");
+    }
+
     @Test
     public void testSuccessfulLogin() {
-        Playwright playwright = Playwright.create();
-        Browser browser = playwright.chromium().launch();
-        Page page = browser.newPage();
-        page.navigate("https://qa-testing.in.devexperts.com/internship/");
-
         Locator inputUsername = page.getByPlaceholder("Username");
         inputUsername.fill("iamritaferraz@gmail.com");
-
-        //Locator inputPassword = page.getByPlaceholder("Password", new Page.GetByPlaceholderOptions().setExact(true)); --by exact placeholder name
-        //as there are two or find per the id attribute, which is unique
         Locator inputPassword = page.locator("#password");
         inputPassword.fill("Internet1!");
-
         Locator buttonLogin = page.getByRole(AriaRole.BUTTON, new Page.GetByRoleOptions().setName("Login")); //find elements by their role on the page. find a button that is named logjn
         buttonLogin.click();
-
         Locator homeHeader = page.locator(".header-title-content");
-        //soft assertions, will only fail after all checks
         assertAll("Login Checks",
                 ()-> assertThat(homeHeader).hasText("Home Test Task"),
                 ()-> assertThat(homeHeader).isVisible());
+    }
+
+    @Test
+    public void testLoginWithWrongCredentials(){
+
+    }
+    @AfterEach
+    public void tearDown(){
         page.close();
         browser.close();
+    }
+    @AfterAll
+    public static void afterAll(){
         playwright.close();
     }
 }
