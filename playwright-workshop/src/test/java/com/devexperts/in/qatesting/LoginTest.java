e8dc932cbb0be358fda130432e303c1286d24073
Alina Tykhonova <atykhonova@devexperts.com>
2024-10-25 12:22:02 +0100

    QEXDEV-358 - Added Successful Login Test (headless: true)

--- playwright-workshop/src/test/java/com/devexperts/in/qatesting/LoginTest.java
+++ playwright-workshop/src/test/java/com/devexperts/in/qatesting/LoginTest.java
@@ -13,7 +13,7 @@ public class LoginTest {
     public void testSuccessfulLogin() {
         //Setup Playwright, Browser , Page
         Playwright playwright = Playwright.create();
-        Browser browser = playwright.chromium().launch(new BrowserType.LaunchOptions().setHeadless(false));
+        Browser browser = playwright.chromium().launch(new BrowserType.LaunchOptions().setHeadless(true));
         Page page = browser.newPage();
         page.navigate("https://qa-testing.in.devexperts.com/internship/");
 
