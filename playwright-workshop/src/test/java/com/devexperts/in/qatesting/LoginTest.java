8f12900dec207d401e95facdff5f9817f061352b
Nickolay Perevalov <nickolay.perevalov@devexperts.com>
2024-10-27 17:17:33 +0100

    String parameters are replaced by PropertiesProvider

--- playwright-workshop/src/test/java/com/devexperts/in/qatesting/LoginTest.java
+++ playwright-workshop/src/test/java/com/devexperts/in/qatesting/LoginTest.java
@@ -1,5 +1,6 @@
 package com.devexperts.in.qatesting;
 
+import com.devexperts.in.qatesting.configuration.PropertiesProvider;
 import com.microsoft.playwright.Browser;
 import com.microsoft.playwright.Locator;
 import com.microsoft.playwright.Page;
@@ -17,11 +18,11 @@ public class LoginTest {
         Playwright playwright = Playwright.create();
         Browser browser = playwright.chromium().launch(new BrowserType.LaunchOptions().setHeadless(false));
         Page page = browser.newPage();
-        page.navigate("https://qa-testing.in.devexperts.com/internship/");
+        page.navigate(PropertiesProvider.getProperty("base.url"));
         Locator inputUsername = page.getByPlaceholder("Username");
-        inputUsername.fill("nickolay.perevalov@devexperts.com");
+        inputUsername.fill(PropertiesProvider.getProperty("test.user"));
         Locator inputPassword = page.locator("#password");
-        inputPassword.fill("MU99%g*9#");
+        inputPassword.fill(PropertiesProvider.getProperty("test.password"));
         Locator loginButton = page.getByRole(AriaRole.BUTTON, new Page.GetByRoleOptions().setName("Login"));
         loginButton.click();
         Locator homeHeaderPage = page.locator(".header-title-content");
