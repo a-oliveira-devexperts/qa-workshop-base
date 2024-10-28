1541caaa18b2aec1d450fcd094452b59304db3bf
BrunoFraga <bfraga@devexperts.com>
2024-10-28 18:41:49 +0000

    QEXDEV-345 - Code refactored with annotations, page objects and used properties

---
+++ playwright-workshop/src/test/java/com/devexperts/in/qa/testing/LoginPage.java
@@ -0,0 +1,50 @@
+package com.devexperts.in.qa.testing;
+
+import com.microsoft.playwright.Locator;
+import com.microsoft.playwright.Page;
+import com.microsoft.playwright.options.AriaRole;
+
+public class LoginPage
+  {
+      Page page;
+      private static final String USERNAME_SELECTOR="#name";
+      private static final String PASSWORD_SELECTOR="#password";
+      private static final String LOGIN_SELECTOR="Login";
+
+      public LoginPage(Page page)
+        {
+           this.page=page;
+        }
+
+      private Locator getInputUsername()
+        {
+           return page.locator(USERNAME_SELECTOR);
+        }
+
+      public void informUsername(String username)
+        {
+           getInputUsername().fill(username);
+        }
+
+
+      private Locator getInputPassword()
+        {
+            return page.locator(PASSWORD_SELECTOR);
+        }
+
+      public void informPassword(String password)
+        {
+           getInputPassword().fill(password);
+        }
+
+      private Locator getButtonLogin()
+        {
+          return page.getByRole(AriaRole.BUTTON, new Page.GetByRoleOptions().setName(LOGIN_SELECTOR));
+        }
+
+      public void clickLogin()
+        {
+
+            getButtonLogin().click();
+        }
+  }         
