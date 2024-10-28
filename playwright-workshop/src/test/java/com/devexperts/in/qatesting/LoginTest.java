2c222bd52079436f87f3f450affe80ed0d835650
Mihail Rangelov <mrangelov@devexperts.com>
2024-10-28 08:26:45 +0200

    [QEXDEV-336] - Refactoring, Added LoginPage.java, tests for wrong credentials and balance check

--- playwright-workshop/src/test/java/com/devexperts/in/qatesting/LoginTest.java
+++ playwright-workshop/src/test/java/com/devexperts/in/qatesting/LoginTest.java
@@ -1,33 +1,86 @@
 package com.devexperts.in.qatesting;
 
+import com.devexperts.in.qatesting.configuration.PropertiesProvider;
 import com.microsoft.playwright.*;
-import com.microsoft.playwright.options.AriaRole;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 
 import static com.microsoft.playwright.assertions.PlaywrightAssertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
+
 public class LoginTest {
+
+    private static Playwright playwright;
+    private Browser browser;
+    private Page page;
+    private static final String USERNAME_DATA = "q@q.q";
+    private static final String PASSWORD_DATA = "Qw#45678";
+    private static final String WRONG_PASSWORD = "Qw#456";
+    private static final String STATUS_MESSAGE = "Wrong password! Correct password is: Qw#45678";
+    private static final String INITIAL_BALANCE = "10000.00";
+
+
+    @BeforeAll
+    public static void BeforeAll(){
+        playwright = Playwright.create();
+    }
+
+    @BeforeEach
+    public void setUp(){
+        browser = playwright.chromium().launch();
+        page = browser.newPage();
+        page.navigate(PropertiesProvider.getProperty("base.url"));
+    }
 
     @Test
     public void testSuccessfulLogin(){
-        Playwright playwright = Playwright.create();
-        Browser browser = playwright.chromium().launch();
-        Page page = browser.newPage();
-        page.navigate("https://qa-testing.in.devexperts.com/internship/");
-        Locator inputUsername = page.getByPlaceholder("Username");
-        inputUsername.fill("mrdxtm4_1@proton.me");
-        Locator inputPassword = page.locator("#password");
-        inputPassword.fill("Qw#45678");
-        Locator loginButton = page.getByRole(AriaRole.BUTTON, new Page.GetByRoleOptions().setName("Login"));
-        loginButton.click();
+
+        LoginPage loginPage = new LoginPage(page);
+        loginPage.informUserName(USERNAME_DATA);
+        loginPage.informPassword(PASSWORD_DATA);
+        loginPage.clickLogin();
+
         Locator homeHeaderPage = page.locator(".header-title-content");
         assertAll("Login Page Checks" ,
                 () ->assertThat(homeHeaderPage).hasText("Home Test Task") ,
-                () -> assertThat(homeHeaderPage).isVisible());
+                () ->assertThat(homeHeaderPage).isVisible());
+    }
 
+    @Test
+    public void testLoginWithWrongCredentials(){
+
+        LoginPage loginPage = new LoginPage(page);
+        loginPage.informUserName(USERNAME_DATA);
+        loginPage.informPassword(WRONG_PASSWORD);
+        loginPage.clickLogin();
+
+        Locator errorMessage = page.locator("#login-status");
+        assertAll("Error Message Checks" ,
+                () ->assertThat(errorMessage).hasText(STATUS_MESSAGE) ,
+                () ->assertThat(errorMessage).isVisible());
+    }
+
+    @Test
+    public void checkBalancesAfterAccountCreation(){
+        LoginPage loginPage = new LoginPage(page);
+        loginPage.informUserName(USERNAME_DATA);
+        loginPage.informPassword(PASSWORD_DATA);
+        loginPage.clickLogin();
+
+        Locator balance = page.locator("#balanceInfoNumber");
+        assertAll("Balance Checks" ,
+                () ->assertThat(balance).hasText(INITIAL_BALANCE),
+                () ->assertThat(balance).isVisible());
+    }
+
+    @AfterEach
+    public void tearDown(){
         page.close();
         browser.close();
+    }
+
+    @AfterAll
+    public static void afterAll(){
         playwright.close();
     }
 }
