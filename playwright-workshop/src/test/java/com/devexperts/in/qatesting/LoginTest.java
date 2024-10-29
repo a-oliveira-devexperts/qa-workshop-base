20332c47f245438e12f8c36e25e4f2b4afac8c99
Elvira Dermendzhieva <edermendzhieva@devexperts.com>
2024-10-29 18:53:01 +0200

    QEXDEV-349 - Added Test Successful Login

---
+++ playwright-workshop/src/test/java/com/devexperts/in/qatesting/LoginTest.java
@@ -0,0 +1,43 @@
+package com.devexperts.in.qatesting;
+
+import com.microsoft.playwright.*;
+import com.microsoft.playwright.assertions.LocatorAssertions;
+import com.microsoft.playwright.options.AriaRole;
+import org.junit.jupiter.api.Test;
+
+import static com.microsoft.playwright.assertions.PlaywrightAssertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertAll;
+
+public class LoginTest {
+
+    @Test
+    public void testSuccessfulLogin(){
+        //Setup Playwright, Browser, Page
+        Playwright playwright = Playwright.create();
+        Browser browser = playwright.chromium().launch(new BrowserType.LaunchOptions().setHeadless(false));
+        Page page = browser.newPage();
+        page.navigate("https://qa-testing.in.devexperts.com/internship/");
+        //Inform username
+        Locator inputUsername = page.getByPlaceholder("Username");
+        inputUsername.fill("edermendzhieva@devexperts.com");
+        //Inform password
+        //Locator inputPassword = page.getByPlaceholder("Password" , new Page.GetByPlaceholderOptions().setExact(true));
+        Locator inputPassword = page.locator("#password");
+        inputPassword.fill("E42ac45D@");
+        //Click in Login Button
+        Locator buttonLogin = page.getByRole(AriaRole.BUTTON , new Page.GetByRoleOptions().setName("Login"));
+        buttonLogin.click();
+        //Check if we were redirected to the homepage
+        Locator homeHeader = page.locator(".header-title-content");
+        //assertThat(homeHeader).hasText("Home Test sdagda Task");
+        //assertThat(homeHeader).isVisible();
+        assertAll("Login Checks",
+                ()-> assertThat(homeHeader).hasText("Home Test Task"),
+                ()-> assertThat(homeHeader).isVisible());
+
+        //Close Page, Browser, Playwright
+        page.close();
+        browser.close();
+        playwright.close();
+    }
+}
