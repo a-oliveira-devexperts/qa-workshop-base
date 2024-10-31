e603dfdd96fe4a046c3c61e76338111d18beb33a
Rita Ferraz <rferraz@devexperts.com>
2024-10-31 12:07:55 +0000

    QEXDEV-354 Implement tests for login with wrong and empty credentials

--- playwright-workshop/src/test/java/com/devexperts/in/qatesting/LoginTest.java
+++ playwright-workshop/src/test/java/com/devexperts/in/qatesting/LoginTest.java
@@ -3,18 +3,17 @@ package com.devexperts.in.qatesting;
 import com.devexperts.in.qatesting.configuration.PropertiesProvider;
 import com.microsoft.playwright.*;
 import org.junit.jupiter.api.*;
-import org.junit.platform.commons.logging.LoggerFactory;
-
 import static com.microsoft.playwright.assertions.PlaywrightAssertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertAll;
 
 public class LoginTest {
-    //private static final Logger log = LoggerFactory.getLogger(LoginTest.class);
     private static Playwright playwright;
     private Browser browser;
     private Page page;
-    private static final String USERNAME_DATA = "iamritaferraz@gmail.com";
-    private static final String PASSWORD_DATA = "Internet1!";
+    private static final String USERNAME_DATA = PropertiesProvider.getProperty("test.user");
+    private static final String PASSWORD_DATA = PropertiesProvider.getProperty("test.password");
+    private static final String USERNAME_WRONG_DATA = PropertiesProvider.getProperty("wrong.test.user");
+    private static final String PASSWORD_WRONG_DATA = PropertiesProvider.getProperty("wrong.test.password");
 
     @BeforeAll
     public static void beforeAll(){
@@ -43,8 +42,41 @@ public class LoginTest {
 
     @Test
     public void testLoginWithWrongCredentials(){
-
+        LoginPage loginPage = new LoginPage(page);
+        loginPage.informUsername(USERNAME_WRONG_DATA);
+        loginPage.informPassword(PASSWORD_WRONG_DATA);
+        loginPage.clickLogin();
+        Locator loginStatus = page.locator("#login-status");
+        assertAll("Login Wrong Credentials",
+                ()->assertThat(loginStatus).isVisible(),
+                ()->assertThat(loginStatus).hasText("Wrong user! User a; not found."));
     }
+
+    @Test
+    public void testLoginWithWrongPassword(){
+        LoginPage loginPage = new LoginPage(page);
+        loginPage.informUsername(USERNAME_DATA);
+        loginPage.informPassword(PASSWORD_WRONG_DATA);
+        loginPage.clickLogin();
+        Locator loginStatus = page.locator("#login-status");
+        assertAll("Login Wrong Password",
+                ()->assertThat(loginStatus).isVisible(),
+                ()->assertThat(loginStatus).hasText("Wrong password! Correct password is: Internet1!"));
+    }
+
+    @Test
+    public void testLoginWithEmptyCredentials(){
+        LoginPage loginPage = new LoginPage(page);
+        loginPage.informUsername(USERNAME_WRONG_DATA);
+        loginPage.informPassword(PASSWORD_DATA);
+        loginPage.clickLogin();
+        Locator loginStatus = page.locator("#login-status");
+        assertAll("Login Empty Credentials",
+                ()->assertThat(loginStatus).isVisible(),
+                ()->assertThat(loginStatus).hasText("Please enter valid credentials:"));
+    }
+
+
     @AfterEach
     public void tearDown(){
         page.close();
