d9dcd3d41b419defbb4e1b8c5b21f31bdf9b3c71
Kirill Pevzner <kpevzner@devexperts.com>
2024-11-01 23:04:11 +0200

    QEXDEV-333 - Removed headless property

--- playwright-workshop/src/test/java/com/devexperts/in/qatesting/configuration/LoginTest.java
+++ playwright-workshop/src/test/java/com/devexperts/in/qatesting/configuration/LoginTest.java
@@ -13,7 +13,7 @@ public class LoginTest {
         //Setup
         //Create Playwright, Browser, Page
         Playwright playwright = Playwright.create();
-        Browser browser = playwright.chromium().launch(new BrowserType.LaunchOptions().setHeadless(false));
+        Browser browser = playwright.chromium().launch(new BrowserType.LaunchOptions().setHeadless(true));
         Page page = browser.newPage();
         //Open Login Page
 
