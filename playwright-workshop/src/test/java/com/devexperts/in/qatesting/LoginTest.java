69381c0b809a4d6ff9cb9c9a8f3de3faa6db1bc3
vsousa <vsousa@devexperts.com>
2025-04-14 12:24:58 +0100

    QEXDEV-362 - Added Login with Wrong Credentials test and Account Creation with Balance validation after Login test

--- playwright-workshop/src/test/java/com/devexperts/in/qatesting/LoginTest.java
+++ playwright-workshop/src/test/java/com/devexperts/in/qatesting/LoginTest.java
@@ -1,34 +1,76 @@
 package com.devexperts.in.qatesting;
 
+import com.devexperts.in.qatesting.configuration.PropertiesProvider;
 import com.microsoft.playwright.*;
 import com.microsoft.playwright.options.AriaRole;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 
 import static com.microsoft.playwright.assertions.PlaywrightAssertions.assertThat;
 import static java.awt.SystemColor.text;
 import static org.junit.jupiter.api.Assertions.*;
 
 public class LoginTest {
+    private static Playwright playwright;
+    private Browser browser;
+    private Page page;
+    private static final String USERNAME_DATA = "vsousa@devexperts.com";
+    private static final String PASSWORD_DATA = "198881^^ZXcvb";
+    private static final String INCORRECT_USERNAME_DATA = "vsou@devexperts.com";
+    private static final String INCORRECT_PASSWORD_DATA = "123456^^ASdfg";
+
+
+    @BeforeAll
+    public static void beforeAll(){
+        playwright = Playwright.create();
+    }
+
+    @BeforeEach
+    public void setUp(){
+        browser = playwright.chromium().launch();
+        page = browser.newPage();
+        page.navigate(PropertiesProvider.getProperty("base.url"));
+    }
+
     @Test
     public void testSuccessfulLogin(){
-        Playwright playwright = Playwright.create();
-        Browser browser = playwright.chromium().launch();
-        Page page = browser.newPage();
-            page.navigate("https://qa-testing.in.devexperts.com/internship/");
-        Locator inputUsername = page.getByPlaceholder("Username" );
-        inputUsername.fill("vsousa@devexperts.com");
-        Locator inputPassword = page.locator( "#password" );
-        inputPassword.fill("198881^^ZXcvb");
-        Locator loginButton = page.getByRole(AriaRole.BUTTON , new Page.GetByRoleOptions().setName("Login"));
-        loginButton.click();
+
+        LoginPage loginPage = new LoginPage(page);
+        loginPage.informUsername(USERNAME_DATA);
+        loginPage.informPassword(PASSWORD_DATA);
+        loginPage.clickLogin();
+
         Locator homeHeaderPage = page.locator(".header-title-content");
         assertAll("Login Page Checks" ,
                 () -> assertThat(homeHeaderPage).hasText("Home Test Task"),
                 () -> assertThat(homeHeaderPage).isVisible());
+    }
 
+
+    @Test
+    public void testLoginWithWrongCredentials() {
+
+        LoginPage loginPage = new LoginPage(page);
+        loginPage.informUsername(INCORRECT_USERNAME_DATA);
+        loginPage.informPassword(INCORRECT_PASSWORD_DATA);
+        loginPage.clickLogin();
+
+        Locator errorMessage = page.locator("#login-status");
+        assertAll("Login Failure Checks",
+                () -> assertTrue(errorMessage.isVisible()),
+                () -> assertThat(errorMessage).hasText("Wrong user! User " + INCORRECT_USERNAME_DATA + " not found."));
+
+    }
+
+
+
+    @AfterEach
+    public void tearDown(){
         page.close();
         browser.close();
+    }
+
+    @AfterAll
+    public static void afterAll(){
         playwright.close();
     }
 }
-
---
+++ playwright-workshop/src/test/java/com/devexperts/in/qatesting/RegistrationPage.java
@@ -0,0 +1,106 @@
+package com.devexperts.in.qatesting;
+
+import com.microsoft.playwright.Locator;
+import com.microsoft.playwright.Page;
+import com.microsoft.playwright.options.AriaRole;
+import com.microsoft.playwright.options.SelectOption;
+import com.microsoft.playwright.options.WaitForSelectorState;
+
+import java.util.Random;
+import java.util.Map;
+import java.util.HashMap;
+
+public class RegistrationPage {
+    Page page;
+    private String selectedBalance;
+    private static final String EMAIL_SELECTOR = "#desiredName";
+    private static final String CREATE_PASSWORD_SELECTOR = "#desiredPassword";
+    private static final String REAL_NAME_SELECTOR = "#desiredRealname";
+    private static final String PHONE_SELECTOR = "#desiredPhone";
+    private static final String BALANCE_SELECTOR = "#desiredDeposit";
+    private static final String OPEN_FORM_BUTTON = ".registrationToggle";
+    private static final String REGISTER_BUTTON = "#registration-button";
+
+    public RegistrationPage(Page page){
+        this.page = page;
+    }
+
+    private Locator getOpenFormButton() {
+        return page.locator(OPEN_FORM_BUTTON, new Page.LocatorOptions().setHasText("Need an account?"));
+    }
+
+    private Locator getInputEmail(){
+        return page.locator(EMAIL_SELECTOR);
+    }
+
+    private Locator getInputPassword(){
+        return page.locator(CREATE_PASSWORD_SELECTOR);
+    }
+
+    private Locator getInputRealName(){
+        return page.locator(REAL_NAME_SELECTOR);
+    }
+
+    private Locator getInputPhone(){
+        return page.locator(PHONE_SELECTOR);
+    }
+
+    private Locator getBalanceDropdown() {
+        return page.locator(BALANCE_SELECTOR);
+    }
+
+    private Locator getButtonRegister(){
+        return page.locator(REGISTER_BUTTON);
+    }
+
+    public void clickOpenForm() {
+        getOpenFormButton().click();
+    }
+
+
+    public void fillEmail(String email) {
+        getInputEmail().fill(email);
+    }
+
+    public void fillPassword(String password) {
+        getInputPassword().fill(password);
+    }
+
+    public void fillRealName(String realName) {
+        getInputRealName().fill(realName);
+    }
+
+    public void fillPhone(String phone) {
+        getInputPhone().fill(phone);
+    }
+
+    public void selectRandomBalance() {
+        Map<String, String> labelToValue = new HashMap<>();
+        labelToValue.put("1.000 BCC", "100000");
+        labelToValue.put("5.000 BCC", "500000");
+        labelToValue.put("10.000 BCC", "1000000");
+
+        String[] balances = {"1.000 BCC", "5.000 BCC", "10.000 BCC"};
+        String selectedLabel = balances[new Random().nextInt(balances.length)];
+
+        this.selectedBalance = labelToValue.get(selectedLabel);
+
+        Locator dropdown = getBalanceDropdown();
+        dropdown.selectOption(new SelectOption().setLabel(selectedLabel));
+    }
+
+    public String getSelectedBalance() {
+        return selectedBalance;
+    }
+
+    public void clickRegister() {
+        getButtonRegister().click();
+    }
+
+    public void waitForPasswordFieldToBeVisible() {
+        page.waitForSelector(CREATE_PASSWORD_SELECTOR, new Page.WaitForSelectorOptions()
+                .setState(com.microsoft.playwright.options.WaitForSelectorState.VISIBLE)
+                .setTimeout(60000));
+    }
+
+}
